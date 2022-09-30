f7113407db6950a9f5e35999c5c94d625352e2b8
Quentin Roggy <quentin.roggy@gmail.com>
2022-09-30 14:37:57 +0200

    :book: docs: Add routes comment for swagger api docs

--- app/routers/api/workspace.js
+++ app/routers/api/workspace.js
@@ -17,36 +17,64 @@ router.use(function(_, res, next) {
 
 /**
  * GET /workspace/find-random
+ * @summary Get 5 random workspace's list
+ * @tags Workspace
+ * @return {ApiError} 400 - Bad request response - application/json
+ * @return {ApiError} 404 - Restaurant not found - application/json
  */
 router.get("/workspace/find-random", controllerHandler(controller.findRandom));
 
 /**
  * GET /workspace/:id
+ * @summary Get a workspace by id
+ * @tags Workspace
+ * @return {ApiError} 400 - Bad request response - application/json
+ * @return {ApiError} 404 - Restaurant not found - application/json
  */
 router.get("/workspace/:id(\\d+)", controllerHandler(controller.findById));
 
 /**
  * GET personalspace/:hostid/workspace
+ * @summary Get workspaces for one host
+ * @tags Workspace
+ * @return {ApiError} 400 - Bad request response - application/json
+ * @return {ApiError} 404 - Restaurant not found - application/json
  */
 router.get("/personalspace/:hostid(\\d+)/workspace", controllerHandler(controller.findWorkspacesByHost));
 
 /**
  * POST /workspace/create
+ * @summary Create a new workspace
+ * @tags Workspace
+ * @return {ApiError} 400 - Bad request response - application/json
+ * @return {ApiError} 404 - Restaurant not found - application/json
  */
 router.post("/workspace/create", controllerHandler(controller.create));
 
 /**
  * POST /workspace/search"
+ * @summary Get all workspaces according to search
+ * @tags Workspace
+ * @return {ApiError} 400 - Bad request response - application/json
+ * @return {ApiError} 404 - Restaurant not found - application/json
  */
 router.post("/workspace/search", controllerHandler());
 
 /**
  * PATCH workspace/:id
+ * @summary Modify a workspace
+ * @tags Workspace
+ * @return {ApiError} 400 - Bad request response - application/json
+ * @return {ApiError} 404 - Restaurant not found - application/json
  */
 router.patch("workspace/:id(\\d+)", controllerHandler());
 
 /**
  * PATCH /workspace/state/:id
+ * @summary Modify availability of one workspace
+ * @tags Workspace
+ * @return {ApiError} 400 - Bad request response - application/json
+ * @return {ApiError} 404 - Restaurant not found - application/json
  */
 router.patch("/workspace/state/:id(\\d+)", controllerHandler(controller.updateState));
 
