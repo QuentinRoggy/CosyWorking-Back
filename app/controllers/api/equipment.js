9516ac4e017a67a100076575d9958c731e9d506a
Quentin Roggy <quentin.roggy@gmail.com>
2022-10-04 08:33:31 +0200

    :rocket: feat: Add route de get all equipments

---
+++ app/controllers/api/equipment.js
@@ -0,0 +1,9 @@
+const equipmentDatamapper = require('../../Datamapper/equipment');
+
+module.exports = {
+  async findAll(_, res) {
+    const result = await equipmentDatamapper.getAll();
+
+    res.json(result);
+  }
+}
---
+++ app/routers/api/equipment.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const controllerHandler = require('../../helpers/controllerHandler');
+const verifyAccesRight = require('../../middleware/verifyAccessRight');
+
+const { equipmentController: controller } = require("../../controllers/api")
+
+// Gives acces to x-access-token in header
+router.use(function(_, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
+
+
+router.get("/equipments", controllerHandler(controller.findAll));
+
+module.exports = router;
