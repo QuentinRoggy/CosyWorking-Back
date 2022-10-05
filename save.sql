3ff66c29a0ba5982f91265b31d0b6d492fb6b5b4
Corentin <corentin.vnqr@gmail.com>
2022-10-05 22:24:40 +0200

    half of seeding done

--- save.sql
+++ save.sql
@@ -1,25 +1,25 @@
 INSERT INTO public."user"(
-	id, last_name, first_name, email, password, username, avatar, about, gender, role_id, created_at, updated_at)
+	last_name, first_name, email, password, username, avatar, about, gender, role_id)
 	VALUES 
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, 2),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, 2),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, 2),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, 2),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, 2),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, 2),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, 1),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, 1),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, 1),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, 1);
+        ('Vanaquer', 'Corentin', 'vanaquer@cosyworking.fr', 'password123$', 'Corentin.V', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/578.jpg', 'Je suis un super hôte', 'male', 2),
+        ('Roggy', 'Quentin', 'roggy@cosyworking.fr', 'password123$', 'Quentin.R', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/578.jpg', 'Je suis primé meilleur hôte de la Maine-et-Loire', 'male', 2),
+        ('Kiwi', 'Jade', 'kiwi@cosyworking.fr', 'password123$', 'Jade.K', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/851.jpg', 'Je suis fan du chocolat milka et de la moutarde de dijon', 'female', 2),
+        ('Liorah', 'Ambre', 'liorah@cosyworking.fr', 'password123$', 'Ambre.L', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1138.jpg', 'Je suis grande, riche et indépendante', 'female', 2),
+        ('Gouttin', 'Paul', 'gouttin@cosyworking.fr', 'password123$', 'Paul.G', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/40.jpg', 'Je suis célibataire, à la recherche de mon âme soeur', 'male', 2),
+        ('Martin', 'Agath', 'martin@cosyworking.fr', 'password123$', 'Agath.M', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1179.jpg', 'Je suis dev nomade et cherche un bureau', 'female', 1),
+        ('Boutoile', 'Pierre', 'boutoile@cosyworking.fr', 'password123$', 'Pierre.B', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/40.jpg', 'Bonjour, je suis gentil avec les chats et les plantes ', 'male', 1),
+        ('Dubois', 'Alice', 'dubois@cosyworking.fr', 'password123$', 'Alice.D', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1050.jpg', 'Salut je prendrais soins de votre bureau acceptez moi please', 'female', 1),
+        ('Nougah', 'Benjamin', 'nougah@cosyworking.fr', 'password123$', 'Benjamin.N', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/764.jpg', 'Hello je cherche des bureaux pour travailler sur un projet secret', 'male', 1),
+        ('Brooks', 'Adelaide', 'brooks@cosyworking.fr', 'password123$', 'Adelaide.B', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1050.jp', 'Hi, I am looking for a place to work with a big pool, work hard play hard!', 'female', 1);
 
 INSERT INTO public.workspace(
 	title, description, address, zip_code, city, longitude, latitude, half_day_price, day_price, availability, user_id)
 	VALUES 
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?),
-        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        ('Le Cocoon', 'Un bureau équipé pour votre bien être, décoré à travers la methode fengshui. Vous y serez épanouis et en sortirez plus heureux que jamais.', '30 rue Saint-Louis', '35000', 'Rennes', '-1.6840268', '48.1133316', 70, 140, true, 1),
+        ('Happy Hour Loft', 'Mon loft est à votre service, vous pourrez y étancher votre soif et bien sûr y travailler comme il se doit', '8 rue Saint-Serge', '49100', 'Angers', '-0.55', '47.4667', 80, 160, true, 2),
+        ('Bureau pas comme les autres', 'Bureau conceptuel dans une ambiance chat Sibérien. Pot de moutarde de Dijon offert', '5 rue Jacques Cellerier', '21000', 'Dijon', '5.0167', '47.3167', 40, 80, true, 3),
+        ('Luxieuse Villa', 'Charmante Villa de 300m2, vous y trouverez une piscine à débordement sur le toit avec vu sur la Tour Effeil ', '3 Av. Anatole', '75007', 'Paris', '2.2966781', '48.8571483', 199, 399, true, 4),
+        ('Charmant Atelier', 'Ancien atelier de poterie aménager en bureau, décoration inspiré du plus grand potier Bernard Palissy', '72 Pl. de Provence', '86000', 'Poitiers', '0.357187', '46.5894948', 30, 60, true, 5);
 
 INSERT INTO public.image(
 	id, link, main_image, workspace_id, created_at, updated_at)
@@ -39,16 +39,21 @@ INSERT INTO public.booking(
         (?, ?, ?, ?, ?, ?, ?, ?, ?),
         (?, ?, ?, ?, ?, ?, ?, ?, ?);
 
+-- DUR
+INSERT INTO public.booking_ref(
+	id)
+	VALUES (?);
+
 --DUR
 INSERT INTO public.equipment(
     description, icon_link)
     VALUES 
-        ('Imprimante', ?),
-        ('Fibre', ?),
-        ('Cuisine', ?),
-        ('Double écran', ?),
-        ('Enceinte', ?),
-        ('Piscine', ?);
+        ('Imprimante', '/path/image/printer'),
+        ('Fibre', '/path/image/fiber'),
+        ('Cuisine', '/path/image/kitchen'),
+        ('Double écran', '/path/image/screen'),
+        ('Enceinte', '/path/image/speaker'),
+        ('Piscine', '/path/image/pool');
 
 
 --DUR
