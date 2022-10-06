7a971546c0fd4da118e359d6e25306aa71e00aa0
Corentin <corentin.vnqr@gmail.com>
2022-10-06 17:02:38 +0200

    correction seedingsql final

--- data/sql_seeding.sql
+++ data/sql_seeding.sql
@@ -9,19 +9,18 @@ INSERT INTO public.role(
 
 
 INSERT INTO public."user"(
-	last_name, first_name, email, password, username, avatar, about, gender, role_id)
-	VALUES 
-        ('Vanaquer', 'Corentin', 'vanaquer@cosyworking.fr', 'Password123$', 'Corentin.V', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/578.jpg', 'Je suis un super hôte', 'male', 2),
-        ('Roggy', 'Quentin', 'roggy@cosyworking.fr', 'Password123$', 'Quentin.R', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/578.jpg', 'Je suis primé meilleur hôte de la Maine-et-Loire', 'male', 2),
-        ('Kiwi', 'Jade', 'kiwi@cosyworking.fr', 'Password123$', 'Jade.K', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/851.jpg', 'Je suis fan du chocolat milka et de la moutarde de dijon', 'female', 2),
-        ('Liorah', 'Ambre', 'liorah@cosyworking.fr', 'Password123$', 'Ambre.L', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1138.jpg', 'Je suis grande, riche et indépendante', 'female', 2),
-        ('Gouttin', 'Paul', 'gouttin@cosyworking.fr', 'Password123$', 'Paul.G', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/40.jpg', 'Je suis célibataire, à la recherche de mon âme soeur', 'male', 2),
-        ('Martin', 'Agath', 'martin@cosyworking.fr', 'Password123$', 'Agath.M', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1179.jpg', 'Je suis dev nomade et cherche un bureau', 'female', 1),
-        ('Boutoile', 'Pierre', 'boutoile@cosyworking.fr', 'Password123$', 'Pierre.B', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/40.jpg', 'Bonjour, je suis gentil avec les chats et les plantes ', 'male', 1),
-        ('Dubois', 'Alice', 'dubois@cosyworking.fr', 'Password123$', 'Alice.D', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1050.jpg', 'Salut je prendrais soins de votre bureau acceptez moi please', 'female', 1),
-        ('Nougah', 'Benjamin', 'nougah@cosyworking.fr', 'Password123$', 'Benjamin.N', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/764.jpg', 'Hello je cherche des bureaux pour travailler sur un projet secret', 'male', 1),
-        ('Brooks', 'Adelaide', 'brooks@cosyworking.fr', 'Password123$', 'Adelaide.B', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1050.jp', 'Hi, I am looking for a place to work with a big pool, work hard play hard!', 'female', 1);
-
+    last_name, first_name, email, password, username, avatar, about, gender, role_id)
+    VALUES 
+          ('Vanaquer', 'Corentin', 'vanaquer@cosyworking.fr', 'Password123$', 'Corentin.V', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/578.jpg', 'Je suis un super hôte', 'male', 2),
+          ('Roggy', 'Quentin', 'roggy@cosyworking.fr', 'Password123$', 'Quentin.R', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/578.jpg', 'Je suis primé meilleur hôte de la Maine-et-Loire', 'male', 2),
+          ('Kiwi', 'Jade', 'kiwi@cosyworking.fr', 'Password123$', 'Jade.K', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/851.jpg', 'Je suis fan du chocolat milka et de la moutarde de dijon', 'female', 2),
+          ('Liorah', 'Ambre', 'liorah@cosyworking.fr', 'Password123$', 'Ambre.L', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1138.jpg', 'Je suis grande, riche et indépendante', 'female', 2),
+          ('Gouttin', 'Paul', 'gouttin@cosyworking.fr', 'Password123$', 'Paul.G', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/40.jpg', 'Je suis célibataire, à la recherche de mon âme soeur', 'male', 2),
+          ('Martin', 'Agath', 'martin@cosyworking.fr', 'Password123$', 'Agath.M', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1179.jpg', 'Je suis dev nomade et cherche un bureau', 'female', 1),
+          ('Boutoile', 'Pierre', 'boutoile@cosyworking.fr', 'Password123$', 'Pierre.B', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/40.jpg', 'Bonjour, je suis gentil avec les chats et les plantes ', 'male', 1),
+          ('Dubois', 'Alice', 'dubois@cosyworking.fr', 'Password123$', 'Alice.D', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1050.jpg', 'Salut je prendrais soins de votre bureau acceptez moi please', 'female', 1),
+          ('Nougah', 'Benjamin', 'nougah@cosyworking.fr', 'Password123$', 'Benjamin.N', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/764.jpg', 'Hello je cherche des bureaux pour travailler sur un projet secret', 'male', 1),
+          ('Brooks', 'Adelaide', 'brooks@cosyworking.fr', 'Password123$', 'Adelaide.B', 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1050.jp', 'Hi, I am looking for a place to work with a big pool, work hard play hard!', 'female', 1);
 
 INSERT INTO public.workspace(
 	title, description, address, zip_code, city, longitude, latitude, half_day_price, day_price, availability, user_id)
@@ -33,39 +32,39 @@ INSERT INTO public.workspace(
         ('Charmant Atelier', 'Ancien atelier de poterie aménager en bureau, décoration inspiré du plus grand potier Bernard Palissy', '72 Pl. de Provence', '86000', 'Poitiers', '0.357187', '46.5894948', 30, 60, true, 5);
 
 
-INSERT INTO public.image(
-	link, main_image, workspace_id)
-	VALUES 
-        -- first
-        ('/public/image/workspace/1', true, 1),
-        ('/public/image/workspace/2', false, 1),
-        ('/public/image/workspace/3', false, 1),
-        ('/public/image/workspace/4', false, 1),
-        ('/public/image/workspace/5', false, 1),
-        -- second
-        ('/public/image/workspace/6', true, 2),
-        ('/public/image/workspace/7', false, 2),
-        ('/public/image/workspace/8', false, 2),
-        ('/public/image/workspace/9', false, 2),
-        ('/public/image/workspace/10', false, 2),
-        -- third
-        ('/public/image/workspace/11', true, 3),
-        ('/public/image/workspace/12', false, 3),
-        ('/public/image/workspace/13', false, 3),
-        ('/public/image/workspace/14', false, 3),
-        ('/public/image/workspace/15', false, 3),
-        -- forth
-        ('/public/image/workspace/16', true, 4),
-        ('/public/image/workspace/17', false, 4),
-        ('/public/image/workspace/18', false, 4),
-        ('/public/image/workspace/19', false, 4),
-        ('/public/image/workspace/20', false, 4),
-        -- fifth
-        ('/public/image/workspace/21', true, 5),
-        ('/public/image/workspace/22', false, 5),
-        ('/public/image/workspace/23', false, 5),
-        ('/public/image/workspace/24', false, 5),
-        ('/public/image/workspace/25', false, 5);
+  INSERT INTO public.image(
+    link, main_image, workspace_id)
+    VALUES 
+          -- first
+          ('https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1103&q=80', true, 1),
+          ('https://images.unsplash.com/photo-1618381801643-3d253a63a386?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80', false, 1),
+          ('https://images.unsplash.com/photo-1618381801643-3d253a63a386?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80', false, 1),
+          ('https://images.unsplash.com/photo-1618381801643-3d253a63a386?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80', false, 1),
+          ('https://images.unsplash.com/photo-1618381801643-3d253a63a386?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80', false, 1),
+          -- second
+          ('https://images.unsplash.com/photo-1497366811353-6870744d04b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80', true, 2),
+          ('https://images.unsplash.com/photo-1535957998253-26ae1ef29506?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80', false, 2),
+          ('https://images.unsplash.com/photo-1535957998253-26ae1ef29506?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80', false, 2),
+          ('https://images.unsplash.com/photo-1535957998253-26ae1ef29506?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80', false, 2),
+          ('https://images.unsplash.com/photo-1535957998253-26ae1ef29506?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80', false, 2),
+          -- third
+          ('https://images.unsplash.com/photo-1585779034823-7e9ac8faec70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80', true, 3),
+          ('https://images.unsplash.com/photo-1556559322-b5071efadc88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80', false, 3),
+          ('https://images.unsplash.com/photo-1556559322-b5071efadc88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80', false, 3),
+          ('https://images.unsplash.com/photo-1556559322-b5071efadc88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80', false, 3),
+          ('https://images.unsplash.com/photo-1556559322-b5071efadc88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80', false, 3),
+          -- forth
+          ('https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', true, 4),
+          ('https://images.unsplash.com/photo-1497215842964-222b430dc094?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', false, 4),
+          ('https://images.unsplash.com/photo-1497215842964-222b430dc094?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', false, 4),
+          ('https://images.unsplash.com/photo-1497215842964-222b430dc094?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', false, 4),
+          ('https://images.unsplash.com/photo-1497215842964-222b430dc094?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', false, 4),
+          -- fifth
+          ('https://images.unsplash.com/photo-1601397210737-a5534480bdc5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', true, 5),
+          ('https://images.unsplash.com/photo-1567987768246-df799f9c8afb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80', false, 5),
+          ('https://images.unsplash.com/photo-1567987768246-df799f9c8afb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80', false, 5),
+          ('https://images.unsplash.com/photo-1567987768246-df799f9c8afb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80', false, 5),
+          ('https://images.unsplash.com/photo-1567987768246-df799f9c8afb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80', false, 5);
 
 
 INSERT INTO public.state(
@@ -129,15 +128,15 @@ INSERT INTO public.booking(
         ('2022-10-21T11:00:00Z', '2022-10-20T15:00:00Z', 10, 5, 2, 5);
 
 
-INSERT INTO public.equipment(
-        description, icon_link)
-        VALUES 
-        ('Imprimante', '/public/image/equipment/printer'),
-        ('Fibre', '/public/image/equipment/fiber'),
-        ('Cuisine', '/public/image/equipment/kitchen'),
-        ('Double écran', '/public/image/equipment/screen'),
-        ('Enceinte', '/public/image/equipment/speaker'),
-        ('Piscine', '/public/image/equipment/pool');
+  INSERT INTO public.equipment(
+    description, icon_link)
+    VALUES 
+    ('Imprimante', 'https://cdn-icons-png.flaticon.com/512/3022/3022251.png'),
+    ('Fibre', 'https://cdn-icons-png.flaticon.com/512/6131/6131198.png'),
+    ('Cuisine', 'https://cdn-icons-png.flaticon.com/512/481/481486.png'),
+    ('Double écran', 'https://cdn-icons-png.flaticon.com/512/3018/3018288.png'),
+    ('Enceinte', 'https://cdn-icons-png.flaticon.com/512/650/650504.png'),
+    ('Piscine', 'https://cdn-icons-png.flaticon.com/512/2932/2932355.png');
 
 
 COMMIT
