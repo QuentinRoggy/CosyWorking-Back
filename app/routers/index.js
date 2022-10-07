1d6caff3f865290ca9f51216d01215793c25ca78
Corentin <corentin.vnqr@gmail.com>
2022-10-07 10:49:22 +0200

    added sanitizer

--- app/routers/index.js
+++ app/routers/index.js
@@ -4,9 +4,10 @@ const apiRouter = require('./api');
 const { errorHandler } = require('../helpers/errorHandler');
 
 const router = express.Router();
+const sanitizer = require('../middleware/sanitizer');
 
 // We prefix our api's route
-router.use('/api', apiRouter);
+router.use('/api', sanitizer, apiRouter);
 
 // We take in our route into handle errors
 router.use((err, _, response, next) => {
